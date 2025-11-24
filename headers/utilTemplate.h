771ea88ea20aa75d6d00b179a707b5e895e9ffd2
Fatima Hernandez <fatima.hernandez1159@alumnos.udg.mx>
2025-11-24 11:29:27 -0600

    Delete headers directory

--- headers/utilTemplate.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-// Template sencillo para mostrar cualquier contenedor con for-each
-// Cumple requisito de usar template
-template <typename Contenedor>
-void mostrarSimple(const Contenedor& c){
-    for(const auto& elem : c){ std::cout << elem << "\n"; }
-}
