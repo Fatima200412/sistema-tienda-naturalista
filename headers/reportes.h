2db9b3f86ae0a2773e89da4dbb7935f93c7d4a3e
Fatima Hernandez <fatima.hernandez1159@alumnos.udg.mx>
2025-11-25 18:14:13 -0600

    Update reportes.h

--- headers/reportes.h
+++ headers/reportes.h
@@ -3,7 +3,7 @@
 #include<iostream>
 #include<string>
 using namespace std;
-//clases que se utilizan en los metodos
+//clases que se utilizan en los metodos 
 class gestorVentas;
 class inventario;
 
@@ -12,5 +12,5 @@ public:
 	reportes() = default;
     string reporteInventario(const inventario& inv) const;
     string reporteVentas(const gestorVentas& gv) const;
-    void exportarReporte(const string& nombreArchivo,const string& contenido) const;
+   
 };
